0e65be1f709461f8e5cd3be9175163f31d5935e1
Ayomide Owolana <ayomideowolana1@gmail.com>
2021-11-20 21:10:09 +0100

    arithmetic functions and operate function created

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
-// add
-// subtract
-// multiply
 // divide
+add =(a,b)=> a + b;
+// subtract
+subtract =(a,b)=> a - b;
+// multiply
+multiply =(a,b)=> a * b;
+// divide
+divide =(a,b)=> a / b;
 
 //operate takes 2 nums and an operator function and calls the function on the two numbers 
+operate=(a,b,func)=> func(a,b);
+
